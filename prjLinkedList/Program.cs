3d24f050f16f0b100db833487842fe051677078e
Aphiwe Mhotwana <127925737+ST10085670@users.noreply.github.com>
2025-07-22 11:47:28 +0200

    Add project files.

---
+++ prjLinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace prjLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node n = new Node(6);
+            Node na = new Node(7);
+            Node nb = new Node(8);
+            n.next = na;
+            na.next = nb;
+            Console.WriteLine(countNode(n));
+        }
+
+        public static int countNode(Node Head) 
+        {
+            int count = 1;
+            Node current = Head;
+            while (current.next != null) 
+            {
+                count++;
+                current = current.next;
+            }
+
+            return count;
+        }
+
+    }
+}
